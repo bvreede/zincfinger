7201b8aecefac79892b294ddea709f6857b7c2a2
Barbara Vreede <b.vreede@gmail.com>
2014-05-14 10:19:03 +0300

    altered so that headline does not end in comma

--- tablenamechange.py
+++ tablenamechange.py
@@ -11,9 +11,11 @@ r=open(db)
 o = open(db[:-4] + "_hc.csv","w")
 
 header=r.next().strip().split(',')	# first line contains table headers; converted into list
+headline = ""
 for h in header:			
 	newh = h.replace(' ','_')	# replaces spaces with underscores
-	o.write(newh + ",")		# converts list back into csv
+	headline += newh + ","		# converts list back to csv, in string
+o.write(headline[:-1])
 o.write("\n")
 
 for l in r:
