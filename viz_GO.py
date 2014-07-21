27136e9f116b61a660d551f5d96d6b1f24d83411
Barbara Vreede <b.vreede@gmail.com>
2014-07-22 02:15:44 +0300

    Marc's adjustments

--- viz_GO.py
+++ viz_GO.py
@@ -1,43 +1,81 @@
+# marc needs to do this for shit to work
+#import matplotlib
+#matplotlib.use('Agg')
+
 import scipy
 import pylab
 import csv
 import scipy.cluster.hierarchy as sch
 
+dbfolder = "/home/barbara/Dropbox/zinc_finger_data/data"
 
 #generate array of csv file
-GOfile = csv.reader(open("/home/barbara/Dropbox/zinc_finger_data/data/results/motifhits_dmel-count.csv")) 
-#GOfile = csv.reader(open("/home/barbara/Dropbox/zinc_finger_data/data/databases/140720-SM00355-dmel-GO-G.csv"))
+# The motifs file path
+GOfile = csv.reader(open("%s/results/motifhits_dmel-count.csv" %(dbfolder))) 
+# the Go terms path
+#GOfile = csv.reader(open("%s/databases/140720-SM00355-dmel-GO-G.csv" %(dbfolder)))
+
 GOmatrix = []
+genes = []
 for line in GOfile:
-	if line[0] == "Gene_stable_ID":
-		continue
-	GOline = []
-	for i in range(3,len(line)-1): #line ends in comma, so this removes the last empty item, and removes the first line
-		GOline.append(float(line[i]))
-	GOmatrix.append(GOline)
+        if line[0] == "Gene_stable_ID":
+            # gather gene labels, skipping the first 3 columns, and 
+            # ignoring the last column (which is '' due to the trailing comma"
+            feature_labels = line[3:]
+            feature_labels = feature_labels[:-1]
+            continue
+        GOline = []
+
+        for i in range(3,len(line)-1): #line ends in comma, so this removes the last empty item, and removes the first line
+            GOline.append(float(line[i]))
+        GOmatrix.append(GOline)
+
+        # add the gene identifier to genes labels
+        genes.append(line[0])
+
+
 D = scipy.array(GOmatrix)
 
-# Compute and plot dendrogram.
-fig = pylab.figure()
-axdendro = fig.add_axes([0.09,0.1,0.2,0.8])
-Y = sch.linkage(D, method='weighted')
-Z = sch.dendrogram(Y, orientation='right')
-axdendro.set_xticks([])
-axdendro.set_yticks([])
+# flippin' da matrix!!!!
+#D = scipy.transpose(D)
 
-# Plot distance matrix.
-axmatrix = fig.add_axes([0.3,0.1,0.6,0.8])
+# Compute and plot dendrogram.
+
+# create a wide figure, sizes are in "inches" WTF???
+fig = pylab.figure(figsize=(25, 10))
+
+# add axes as: left bottom width height
+axdendro = fig.add_axes([0.09,0.1,0.2,0.8])
+
+Y = sch.linkage(D, method='weighted')
+Z = sch.dendrogram(Y, orientation='left') #labels=feature_labels, orientation='right')
+
+# get correct row label orders
 index = Z['leaves']
 D = D[index,:]
-D = D[:,index]
-im = axmatrix.matshow(D, aspect='auto', origin='lower')
-axmatrix.set_xticks([])
-axmatrix.set_yticks([])
+#D = D[:,index]
+
+axdendro.set_xticks([])
+
+# Plot distance matrix.
+x_start = 0.45
+axmatrix = fig.add_axes([x_start, 0.1,1-x_start-0.15,0.8])
+
+im = axmatrix.matshow(D, aspect='auto', origin='right', cmap='PuRd')
+
+# hide all t ticks
+#axmatrix.set_yticks([])
+#axmatrix.set_xticks([])
+
+# draw super tiny gene names here
+#axmatrix.set_xticks(range(len(genes)))
+#axmatrix.set_xticklabels(genes, rotation=270, fontsize=0.2)
 
 # Plot colorbar.
 axcolor = fig.add_axes([0.91,0.1,0.02,0.8])
 pylab.colorbar(im, cax=axcolor)
 
 # Display and save figure.
-fig.show()
-fig.savefig("dendrogram.png")
+# fig.show()
+fig.savefig("dendrogram.png", dpi=1200)
+
