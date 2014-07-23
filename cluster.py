a1e068874a820f69157926da6b1001607fceb042
Barbara Vreede <b.vreede@gmail.com>
2014-07-23 08:15:10 +0300

    cleaned up code

--- cluster.py
+++ cluster.py
@@ -3,48 +3,43 @@ import scipy.cluster.hierarchy as sch
 from collections import Counter
 
 dbfolder = "/home/barbara/Dropbox/zinc_finger_data/data"
+inputfile = csv.reader(open("%s/results/motifhits_dmel-count.csv" %(dbfolder))) 
 
-#generate array of csv file
-# The motifs file path
-GOfile = csv.reader(open("%s/results/motifhits_dmel-count.csv" %(dbfolder))) 
-# the Go terms path
-#GOfile = csv.reader(open("%s/databases/140720-SM00355-dmel-GO-G.csv" %(dbfolder)))
-
-GOmatrix = []
-genes = []
-feature_labelsU = []
-for line in GOfile:
-        if line[0] == "Gene_stable_ID":
-            # gather gene labels, skipping the first 3 columns, and 
-            # ignoring the last column (which is '' due to the trailing comma"
-            feature_labelsT = line[3:] #for labels of inverse/transposed 
-            feature_labelsT = feature_labelsT[:-1]
+'''
+Read the input file and extract:
+- matrix of observations (GOmatrix)
+- labels of GOterms or motifs
+- labels of genes (both numbers and names)
+provides options to transpose the matrix
+'''
+observations = [] #will collect the actual matrix of observations
+genenumbers = [] #will collect gene numbers (column 1)
+genenames = [] #will collect gene names
+for line in inputfile:
+        if line[0] == "Gene_stable_ID": # gather GOterm/motif labels
+            termlabels = line[3:] # skipping the first 3 columns
+            termlabels = termlabels[:-1] # ignoring the last column (which is '' due to the trailing comma)
             continue
-	feature_labelsU.append(line[1]) #for labels of untransposed matrix: gene names
-        GOline = []
-
-        for i in range(3,len(line)-1): #line ends in comma, so this removes the last empty item, and removes the first line
+	genenumbers.append(line[0])
+	genenames.append(line[1])
+        GOline = [] #collects the observations per line
+        for i in range(3,len(line)-1): #line ends in comma, so this removes the last empty item, and removes the first identifiers
             GOline.append(float(line[i]))
-        GOmatrix.append(GOline)
+        observations.append(GOline)
+GOmatrix = scipy.array(observations) # matrixify the observations
+#GOmatrix = scipy.transpose(GOmatrix) # flips the matrix --- comment out if necessary!
 
-        # add the gene identifier to genes labels
-        genes.append(line[0])
+'''
+Calculate the clusters. Uses a cutoff value to define the number
+of clustered categories that will be made.
+'''
+cutoff = 25 # determine cutoff: number of categories to be formed(1.1547)
+L = sch.fclusterdata(GOmatrix, cutoff, criterion='maxclust', method='weighted') 
+S = set(L) #turns the clustering into a set so as to remove duplicates
+Llist = list(L) #turns the clustering into a list, so it may be indexed
 
 
-D = scipy.array(GOmatrix)
-Y = sch.linkage(D, method='weighted')
-cutoff = 25#1.1547
-#Z = sch.dendrogram(Y, labels=feature_labelsU)#, orientation='right')#labels=feature_labels, orientation='right')
-
-#T=sch.fcluster(Y, cutoff,'distance')
-#print T
-#Z=sch.dendrogram(Y, color_threshold=cutoff)
-
-L = sch.fclusterdata(D, cutoff, criterion='maxclust', method='weighted') #criterion='inconsistent', metric='euclidean', depth=2, , R=None) #
-S = set(L)
-Llist = list(L)
 print len(S)
-
 print Counter(Llist)
 '''
 for i in range(len(S)):
