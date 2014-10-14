3e055a0621f33138534a420352beb56469b0960d
Barbara Vreede <b.vreede@gmail.com>
2014-10-14 19:58:26 +0300

    cluster array of jaro distances

--- cluster.py
+++ cluster.py
@@ -12,7 +12,7 @@ infile = "%s/databases/GO_%s_old.csv" %(dbfolder,species) #the file to apply the
 inputfile = open(motiffile)
 clustermeth = "weighted"
 clusterorder = "%s/results/clustering-string_%s-%s.csv" %(dbfolder,species,clustermeth)
-cutoff = 50
+cutoff = 0.25
 
 orderfile = open(clusterorder, "w")
 
@@ -32,43 +32,50 @@ for line in inputfile:
 		header = line.strip()[1:].split('|')
 		genenumbers.append(header[0])
 		genenames.append(header[1])
-	elif len(line.strip()) > 0:
+		flag = 1
+	elif flag == 1:
 		observations.append(line.strip())
-#GOmatrix = scipy.array(observations) # matrixify the observations
+		flag = 0
 
 # set up the measurement of jaro distance of a combination of words
 def wordcomp(coord):
 	i,j = coord
-	return 1-jeljar(observations[i],observations[j])
+	return 1 - jeljar(observations[i],observations[j])
 
 # make the array of coordinates for reciprocal comparisons
 ar = nti(len(observations),0)
+print len(ar)
 
 # actually run the comparisons over the entire length of the array
 matrix = naaa(wordcomp,0,ar)
-
+print len(matrix)
 
 '''
 Calculate the clusters. Uses a cutoff value to define the number
 of clustered categories that will be made.
 '''
 C = sch.linkage(matrix, method=clustermeth)
+L = sch.fcluster(C,cutoff,criterion='distance')
 
-L = sch.fcluster(C,cutoff,criterion='inconsistent')
-
-print L
 S = set(L) #turns the clustering into a set so as to remove duplicates
 Llist = list(L) #turns the clustering into a list, so it may be indexed
-print Counter(Llist) #counts instances per category
-
 
 '''
+Reporting on the results:
+'''
+print "Number of categories: ", len(S)
+print "Instances per category: ", Counter(Llist) #counts instances per category
+print "Number of genenumbers/genenames: ", len(genenumbers), len(genenames)
+print "Number of strings for clustering: ", len(observations)
+print "Number of items assigned categories: ", len(L), len(Llist)
+
 cldict = {}
 for i in range(len(genenumbers)):
-	orderfile.write("%s,%s\n" %(genenumbers[i],Llist[i]))
+	orderfile.write("%s,%s,%s,%s\n" %(genenumbers[i],genenames[i],observations[i],Llist[i]))
 	cldict[genenumbers[i]] = Llist[i]
 orderfile.close()
 
+'''
 #for each category, go into GO and motif files, and save the genes seperately
 for c in range(1,cutoff+1):
 	outfile = infile[:-4] + "-cluster" + str(c) + '.csv'
