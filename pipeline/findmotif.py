2c16ea1230617ede2054a5830a777c654ab6a801
Barbara Vreede <b.vreede@gmail.com>
2015-01-22 13:28:05 +0200

    remaining comit

--- pipeline/findmotif.py
+++ pipeline/findmotif.py
@@ -16,16 +16,22 @@ import numpy as np
 
 ### SPECIFY INFORMATION: DATA TO USE###
 ### input consists of: dbfolder/seqfolder/prefix-species_suffix
-### output consists of: dbfolder/resfolder/
+### output consists of: dbfolder/resfolder/ except for sequences that go into seqfolder
 dbfolder = "/home/barbara/Dropbox/shared_work/zinc_finger_data/data"
 seqfolder = "sequences"
 resfolder = "results"
+imgfolder = "images"
 prefix = "150111-SM00355"
 suffix = "seq.fasta"
 species = ["dmel","tcas","dpul","isca","smar","turt"]
-motiflist = ['2_8_3','2_8_4','2_8_5','2_8_6','4_8_3','4_8_4','4_8_5','4_8_6','2_12_3','2_12_4','2_12_5','2_12_6','4_12_3','4_12_4','4_12_5','4_12_6','2_15_3','2_15_4','2_15_5','2_15_6','4_15_3','4_15_4','4_15_5','4_15_6'] # use only numbers, indicating the distances between C-C-H-H (separated by _)
+#define motifs: first a complete dataset
+moCC = [2,3,4] #distances between CC
+moCH = [7,8,9,10,11,12,13,14,15] #distances between CH
+moHH = [2,3,4,5,6] #distances between HH
+motiflist = ['%s_%s_%s' %(m,n,o) for m in moCC for n in moCH for o in moHH] # turn this on if you want to search for all possible combinations of the above.
+motiflist = ['2_7_4','2_8_3','2_9_3','2_10_5','2_11_3','2_11_4','2_12_2','2_12_3','2_12_4','2_12_5','2_12_6','2_13_3',
+'2_13_4','2_14_3','2_14_4','2_15_4','3_8_3','4_12_3','4_12_4','4_15_3']
 #HFresidues = ['V','I','L','M','F','W','C','A','Y','H','T','S','P','G','R','K'] #hydrophobic residues.
-
 
 '''
 Define regular expressions for all zf-domains (by the C-H distances), and save in a
@@ -48,7 +54,7 @@ for m in motiflist:
 Motif to cluster-able sequence: make a dictionary to translate
 the zf-domains to a single letter
 '''
-alphabet = "ABCDEFGHIJKLMNPQRSTUVWXY"
+alphabet = """ABCDEFGHIJKLMNPQRSTUVWXYabcdefghijklmnopqrstuvwxy1234567890!@%^&*()_-+={}[]:;"'|\<,>.?/~`ABCDEFGHIJKLMNPQRSTUVWXYabcdefghijklmnopqrstuvwxy1234567890!@%^&*()_-+={}[]:;"'|\<,>.?/~`"""
 translationdict = {motif: alphabet[a] for a,motif in enumerate(motiflist)} # dictionary of motifs and the corresponding string element
 
 '''
@@ -62,7 +68,7 @@ combodict = {a: 0 for a in set([frozenset([m,n]) for m in motiflist for n in mot
 Open files to save the sequences of all hits per motif
 '''
 for m in motiflist:
-	mfile = open("%s/%s/%s.txt" %(dbfolder,resfolder,m), "w")
+	mfile = open("%s/%s/%s.txt" %(dbfolder,seqfolder,m), "w")
 	mfile.close()
 
 '''
@@ -163,7 +169,7 @@ for sp in species:
 
 		# check each motif individually
 		for m in motifdict: #go through each motif and find all instances in the sequence
-			mfile = open("%s/%s/%s.txt" %(dbfolder,resfolder,m), "a")
+			mfile = open("%s/%s/%s.txt" %(dbfolder,seqfolder,m), "a")
 			domain = motifdict[m]
 			CC,CH,HH = m.split('_')
 			for i in domain.finditer(fastadict[key]):
@@ -272,5 +278,5 @@ ax.set_xticklabels(motiflist, minor=False, rotation=90)
 ax.set_yticklabels(motiflist, minor=False)
 
 #pl.show()
-pl.savefig("%s/%s/heatmap_%s.svg" %(dbfolder,resfolder,colourformap), dpi = 300)
+pl.savefig("%s/%s/heatmap_%s.svg" %(dbfolder,imgfolder,colourformap), dpi = 300)
 
