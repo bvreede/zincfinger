a4fd43712187b1cb99b57ad4890d508d34770bba
Barbara Vreede <b.vreede@gmail.com>
2015-04-01 16:39:31 +0200

    added comments

--- pipeline/clusters2image.py
+++ pipeline/clusters2image.py
@@ -1,15 +1,20 @@
+#!/usr/bin/python
+
+'''
+This script can be used to take a csv file where clusters are indicated (in column 4 as a number) and
+use it to make individual fasta files and visualizations of the genes in this cluster.
+Author: Barbara Vreede
+Contact: b.vreede@gmail.com
+Date: 1 April 2015
+'''
+
 import csv, sys
 
 maindb = "/home/barbara/Dropbox/shared_work/zinc_finger_data/data/results/interest_clusters"
-infile = "nameclusters4.csv"
+infile = "nameclusters4.csv" #csv file with [geneID,genename,proteinID,clusternumber]
 
-#fasta files with sequences
-fasta1 = "/home/barbara/Dropbox/shared_work/zinc_finger_data/data/sequences/150111-SM00355-" #prefix
-fasta2 = "_seq-trans.fasta" #postfix
-spp = ['turt','tcas','dmel','isca','smar','dpul']
-allfasta = ["%s%s%s" %(fasta1,s,fasta2) for s in spp]
-
-
+#fasta file with sequences
+fasta = "/home/barbara/Dropbox/shared_work/zinc_finger_data/data/sequences/150111-SM00355-allz_seq.fasta"
 
 '''
 Read the input from the cluster file (= gene names and their assigned cluster)
@@ -42,17 +47,32 @@ if len(singlecluster) >= 1:
 '''
 Save all fasta files to memory so sequences can be recalled per cluster.
 '''
-sequences = {}
-for f in allfasta:
-	fo = open(f)
+fastadict = {}
+def fastadicter(fastadb):
+	'''
+	Takes an open fastafile and adds the sequences to a general sequences dictionary, with the
+	headers as keys.
+	'''
+	global fastadict
+	sequence = ""
+	header = ""
+	for line in fastadb:
+		if line[0] == ">":
+			if header != "":
+				fastadict[header] = sequence
+			header = line[1:].strip()
+			sequence = ""
+		else:
+			sequence += line.strip()
+		fastadict[header] = sequence
+
+fo = open(fasta)
+fastadicter(fo)
 
 
 '''
-
-input file: nameclusters4
-
 output:
-- per cluster: fasta file with sequences (send to Ariel and have him make sequence trees)
+CHECK per cluster: fasta file with sequences (send to Ariel and have him make sequence trees)
 - per cluster: motif scores in separate file (and run the viz script on this)
 - Drosophila genes only; gorilla input list
 - Drosophila genes only; with gorilla output: make visualization (heatmap)
@@ -61,4 +81,15 @@ output:
 
 for i,c in enumerate(clusterinfo):
 	clusterid = clusternames[i]
+	#generate a fastafile per cluster with only the genes from this cluster
+	cfile = "%s/cluster-%s.fa" %(maindb,clusterid)
+	out = open(cfile, "w")
+	for n in c:
+		name = "%s|%s|%s" %(n[0],n[1],n[2])
+		out.write(">%s\n" %name)
+		out.write(fastadict[name])
+		out.write("\n\n")
+	out.close()
 	
+
+
