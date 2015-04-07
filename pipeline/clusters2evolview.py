0c33d42ff53bd4c9f8fb4a63bd619e71a0cc7385
Barbara Vreede <b.vreede@gmail.com>
2015-04-07 14:42:08 +0200

    adjusted colour scheme

--- pipeline/clusters2evolview.py
+++ pipeline/clusters2evolview.py
@@ -1,10 +1,11 @@
 import csv
 
 in1 = csv.reader(open("/home/barbara/Dropbox/shared_work/zinc_finger_data/data/results/interest_clusters/nameclusters1.csv"))
-in2 = csv.reader(open("/home/barbara/Dropbox/shared_work/zinc_finger_data/data/results/interest_clusters/nameclusters4.csv"))
-out = open("/home/barbara/Dropbox/shared_work/zinc_finger_data/data/results/interest_clusters/evolview_oldclusters.txt", "w")
+in2 = csv.reader(open("/home/barbara/Dropbox/shared_work/zinc_finger_data/data/results/interest_clusters/nameclusters6.csv"))
+out = open("/home/barbara/Dropbox/shared_work/zinc_finger_data/data/results/interest_clusters/evolview_newclusters.txt", "w")
 
-colourdict = {'1': '#6fc833', '2': '#ff75cf', '3': '#ff8c00', '4': '#ffab91', '5': '#476291', '6': '#8650ac', '7': '#0a6910', '8': '#5092ce', '9': '#bd1f30', '10': '#0eb0ff', '11': '#ffd700', '12': '#addbe1', '13': '#1e3072', '14': '#830000'}
+
+colourdict = {'1': '#6fc833', '2': '#ff75cf', '3': '#ff8c00', '4': '#ffd700', '5': '#1e3072', '6': '#8650ac', '7': '#0a6910', '8': '#5092ce', '9': '#0eb0ff', '10': '#bd1f30', '11': '#ffab91', '12': '#addbe1', '13': '#476291', '14': '#830000'}
 
 out.write("## leaf background color\n\n")
 
@@ -20,6 +21,7 @@ for line in in2:
 	name = name.replace(':','-')
 	colour = colourdict[line[3]]
 	out.write("%s\t%s\tprefix\n" %(name,colour))
+
 
 out.close()
 
