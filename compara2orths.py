2164587b9562c6df87793c9fa922c525d93a554f
Barbara Vreede <b.vreede@gmail.com>
2015-06-11 14:11:49 +0300

    changed dictionary setup to orth pairs

--- compara2orths.py
+++ compara2orths.py
@@ -3,15 +3,15 @@ import urllib2, os, config, sys, re
 
 ### OPTIONS ###
 saving = 0 # Set to 1 if you want to save the compara data locally as a text doc.
-parseonline = 0 # Set to 1 if you want to parse ONLINE compara data
-parselocal = 1 # Set to 1 if you want to parse LOCAL compara data (as a text file)
+parseonline = 1 # Set to 1 if you want to parse ONLINE compara data
+parselocal = 0 # Set to 1 if you want to parse LOCAL compara data (as a text file)
 
 
 
 # spp list (in sequence)
-spp = ['isca','dmel','tcas','dpul','smar','turt','cele','atri','atha','crei','cmer','osat','ppat','smoe','slyc','aque','bmal','hrob','lgig','mlei','nvec','sman','spur','tadh','bnat','ehux','glam','gthe','lmaj','pinf','pfal','tthe','mmus','drer','ggal','hsap','mmus','xtro']
+spp = ['dmel','tcas','isca','dpul','smar','turt','cele','atri','atha','crei','cmer','osat','ppat','smoe','slyc','aque','bmal','hrob','lgig','mlei','nvec','sman','spur','tadh','bnat','ehux','glam','gthe','lmaj','pinf','pfal','tthe','mmus','drer','ggal','hsap','mmus','xtro']
 
-spp = ['dmel','smar']
+#spp = ['dmel','smar']
 
 vertebrates = ['mmus','drer','ggal','hsap','mmus','xtro']
 
@@ -19,7 +19,9 @@ ctype = "pan_homology"
 seqfolder = "%s/%s" %(config.mainfolder,config.seqfolder)
 comparafolder = "%s/compara" %config.mainfolder
 
+# regular expressions to search for in json results
 proteinre = re.compile('protein_id.*taxon_id')
+sppre = re.compile('species.*perc_id')
 
 
 def filelist(spp):
@@ -67,7 +69,7 @@ def comparahtml(gene,sp):
 	if sp in vertebrates:
 		url = "http://rest.ensembl.org/homology/id/%s?compara=%s&content-type=application/json" %(gene,ctype) #vertebrate url
 	else:
-		url = "http://rest.ensemblgenomes.org/homology/id/%s?compara=%s&content-type=application/json" %(genename,ctype) #metazoa, plants, protists url
+		url = "http://rest.ensemblgenomes.org/homology/id/%s?compara=%s&content-type=application/json" %(gene,ctype) #metazoa, plants, protists url
 	response = urllib2.urlopen(url)
 	html = response.read()
 	return html
@@ -84,14 +86,27 @@ def savecompara(html,sp,gene):
 
 def findorthos(txt):
 	'''
-	Goes through compara json file to return orthologs in a dictionary.
+	Goes through compara json file to return orthologs in a dictionary
+	where keys are proteins and values are the corresponding species.
 	'''
 	orthosdict = {}
 	txtli = txt.split("source")[1:] #splits each ortholog hit (removing the first one, which contains text prior to the source statement)
 	for source in txtli:
-		entry = source.split("target")[1] #picks the part of the hit that contains species and gene ID information
-		result = proteinre.search(entry)
-		print result.group()
+		# get separate entries for source and ortholog
+		sourceentry,orthoentry = source.split("target")
+		# in the entry for the ortholog, identify the species
+		ospfind = sppre.search(orthoentry)
+		ospecies = ospfind.group()[10:-10]
+		osp_abbr = ospecies[0] + ospecies.split('_')[1][:3]
+		if osp_abbr not in spp: # checks if we're interested in this species at all
+			continue
+		# in the entry for the source, identify the protein ID
+		sprotfind = proteinre.search(sourceentry)
+		sprotein = sprotfind.group()[13:-11]
+		# in the entry for the ortholog, identify the protein ID	
+		oprotfind = proteinre.search(orthoentry)
+		oprotein = oprotfind.group()[13:-11]
+		orthosdict[osp_abbr + '_' + oprotein] = sprotein
 	return orthosdict
 
 def parsecompara(txt):
@@ -112,8 +127,6 @@ if __name__ == "__main__":
 	for k,sp in enumerate(spp):
 		infile = "%s/%s" %(seqfolder,fastas[k])
 		genes,names,proteins,prot2gene = fastaheaders(infile) #read fasta file 
-		# open file, write headers
-		#out = ""
 		for gene in genes: # per gene
 			print "Reading compara for %s in %s..." %(gene,sp)
 			#out.write(gene) # write gene in output
@@ -122,21 +135,19 @@ if __name__ == "__main__":
 			# save or parse?
 			if saving == 1:
 				savecompara(html,sp,gene)
-				print "Saved file."
 			if parseonline == 1:
-				orthoscsv = parsecompara(html) # to parse directly from online file
-				print "Parsed online data."
+				orthosdict = findorthos(html) # to parse directly from online file
+				print orthosdict
 			if parselocal == 1:
 				try:
 					txtfile = open("%s/%s-%s.txt" %(comparafolder,sp,gene))
 					txt = ""
 					for line in txtfile:
 						txt += line
-					orthoscsv = parsecompara(txt) # to parse from a local file
-					print "Parsed local data."
+					orthosdict = findorthos(txt) # to parse from a local file
 				except IOError:
 					print "No file found for %s in %s." %(gene,sp)
-					orthoscsv = ""
-			if parseonline == 1 or parselocal == 1:
-				# write orthos in output
-				print "Output added."
+					continue
+			#if parseonline == 1 or parselocal == 1:
+			#	for sp2 in spp:
+					
