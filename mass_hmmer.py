a25fabb30f5315f466e2eb36d7a37b6acd7e0f30
Barbara Vreede <b.vreede@gmail.com>
2015-06-11 22:56:40 +0300

    changed command

--- mass_hmmer.py
+++ mass_hmmer.py
@@ -11,6 +11,11 @@ spp2 = ['bnat','ehux','glam','gthe','lmaj','pinf','pfal','tthe']
 
 
 spp2 = ['dmel','tcas','isca','dpul','smar','turt']
+
+
+sppall = ['dmel','tcas','isca','dpul','smar','turt','atri','atha','crei','cmer','osat','ppat','smoe','slyc','aque','bmal','cele','hrob','lgig','mlei','nvec','sman','spur','tadh','drer','ggal','hsap','mmus','xtro']
+
+
 '''
 for sp in spp1:
 	#command = "python findmotif2.py /home/barbara/Dropbox/shared_work/zinc_finger_data/newdata/sequences/150111-SM00355-%s_seq.fasta" %sp
@@ -21,9 +26,9 @@ for sp in spp1:
 '''
 
 
-for sp in spp2:
-	#command = "python findmotif2.py /home/barbara/Dropbox/shared_work/zinc_finger_data/newdata/sequences/150602-SM00355-%s_seq.fasta" %sp
-	command = "/Users/BarbaraMaria/Downloads/hmmer-3.1b2-macosx-intel/binaries/hmmsearch -o ~/Dropbox/zinc_finger_data/newdata/results/150602-SM00355-%s_hmmsearch.txt --incT 3.0 /Users/BarbaraMaria/Dropbox/zinc_finger_data/newdata/sequences/zf-C2H2.hmm /Users/BarbaraMaria/Dropbox/zinc_finger_data/newdata/sequences/150602-SM00355-%s_seq.fasta" %(sp,sp)
+for sp in sppall:
+	command = "python findmotif2.py /home/barbara/Dropbox/shared_work/zinc_finger_data/newdata/sequences/150602-SM00355-%s_seq.fasta" %sp
+	#command = "/Users/BarbaraMaria/Downloads/hmmer-3.1b2-macosx-intel/binaries/hmmsearch -o ~/Dropbox/zinc_finger_data/newdata/results/150602-SM00355-%s_hmmsearch.txt --incT 3.0 /Users/BarbaraMaria/Dropbox/zinc_finger_data/newdata/sequences/zf-C2H2.hmm /Users/BarbaraMaria/Dropbox/zinc_finger_data/newdata/sequences/150602-SM00355-%s_seq.fasta" %(sp,sp)
 	os.system(command)
-	#print "motifs identified for", sp
-	print "hmm screen done for", sp
+	print "motifs identified for", sp
+	#print "hmm screen done for", sp
