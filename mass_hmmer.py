5ad1026f5333dfe841b59e1b46885628269a57c7
Barbara Vreede <b.vreede@gmail.com>
2015-06-12 22:19:31 +0300

    cleaned up species lists

--- mass_hmmer.py
+++ mass_hmmer.py
@@ -1,20 +1,19 @@
 #!/bin/python
 import os
 
-spp1 = ['dmel2','tcas2','dpul2','isca2','smar2','turt2','arth2']
+#individual groups
+arth = ['drer','ggal','hsap','mmus','xtro'] #arth
+chor = ['dmel','tcas','isca','dpul','smar','turt'] #chor
+eani = ['nvec','mlei','tadh','sman','aque'] #eani
+plan = ['atri','ppat','crei','atha','slyc','osat','smoe','cmer'] #plan
+prot = ['pfal','bnat','tthe','gthe','lmaj','ehux','pinf','glam'] #prot
+anim = ['hrob','spur','lgig','bmal','cele','drer','ggal','hsap','mmus','xtro','dmel','tcas','isca','dpul','smar','turt','nvec','mlei','tadh','sman','aque']
 
+#all species
+sppall = ['pfal','bnat','tthe','gthe','lmaj','ehux','pinf','glam','dmel','tcas','isca','dpul','smar','turt','atri','atha','crei','cmer','osat','ppat','smoe','slyc','aque','bmal','cele','hrob','lgig','mlei','nvec','sman','spur','tadh','drer','ggal','hsap','mmus','xtro']
 
-
-spp2 = ['atri','atha','crei','cmer','osat','ppat','smoe','slyc','aque','bmal','cele','hrob','lgig','mlei','nvec','sman','spur','tadh','drer','ggal','hsap','mmus','xtro']
-
-spp2 = ['bnat','ehux','glam','gthe','lmaj','pinf','pfal','tthe']
-
-
-spp2 = ['dmel','tcas','isca','dpul','smar','turt']
-
-
-sppall = ['pfal','bnat','tthe','gthe','lmaj','ehux','pinf','glam']#['dmel','tcas','isca','dpul','smar','turt','atri','atha','crei','cmer','osat','ppat','smoe','slyc','aque','bmal','cele','hrob','lgig','mlei','nvec','sman','spur','tadh','drer','ggal','hsap','mmus','xtro']
-
+#species in concatenated files
+sppcombi = ['arth','chor','eani','anim','prot','plan']
 
 '''
 for sp in spp1:
@@ -25,10 +24,11 @@ for sp in spp1:
 	print "hmm screen done for", sp
 '''
 
-
+count = 0
 for sp in sppall:
+	count += 1
 	command = "python findmotif2.py /home/barbara/Dropbox/zinc_finger_data/newdata/sequences/150602-SM00355-%s_seq.fasta" %sp
 	#command = "/Users/BarbaraMaria/Downloads/hmmer-3.1b2-macosx-intel/binaries/hmmsearch -o ~/Dropbox/zinc_finger_data/newdata/results/150602-SM00355-%s_hmmsearch.txt --incT 3.0 /Users/BarbaraMaria/Dropbox/zinc_finger_data/newdata/sequences/zf-C2H2.hmm /Users/BarbaraMaria/Dropbox/zinc_finger_data/newdata/sequences/150602-SM00355-%s_seq.fasta" %(sp,sp)
 	os.system(command)
-	print "motifs identified for", sp
+	print "%s: motifs identified for %s." %(count,sp)
 	#print "hmm screen done for", sp
