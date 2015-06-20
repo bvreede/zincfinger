212a903a49a59726898f0e569297d9528c295c85
Barbara Vreede <b.vreede@gmail.com>
2015-06-20 23:50:16 +0300

    completed ambiguous motif screen with output

--- orthconservation-part2.py
+++ orthconservation-part2.py
@@ -7,6 +7,8 @@ if len(sys.argv) <= 1:
 infile = sys.argv[1]
 infilebrev = infile.split('/')[-1].split('_')[0]
 
+### OUTPUT FILES ###
+heatmaptxt = "%s/%s/%s_comservation-heatmap" %(config.mainfolder,config.evfolder,infilebrev)
 
 orthin = [line for line in csv.reader(open(infile))]
 
@@ -39,6 +41,29 @@ def re_move(s):
 	sli = s.split('|')
 	sli_pairs = [frozenset(list(pair)) for pair in itertools.combinations(sli,2)]
 	return sli_pairs
+
+def makeevolviewheatmap(doublematrix,name):
+	'''
+	Takes a list of lists (heatmap data) and uses motifs as labels
+	and returns a text file that can be read by evolview.
+	'''
+	# Open the heatmap text file:
+	evhm = open("%s-%s.txt" %(heatmaptxt,name), "w")
+	evhm.write(" #heatmap\n !legendTitle\tFrequency of overlap\n !showLegends\t1\n !colorgradient\tfloralwhite,orange,red,purple,blue\
+\n !colorgradientMarkLabel\t0,0.2,0.4,0.6,0.8,1\n # -- heatmap column labels --\n !showHeatMapColumnLabel\t1\n !heatmapColumnLabels\t")
+	# heatmap requires motiflist in sequence, with commas between them
+	motifscomma = ','.join(config.motiflist) 
+	evhm.write("%s\n" %motifscomma)
+	evhm.write(" # -- heatmap --\n !heatmap\tmargin=1,colwidth=18,roundedcorner=1\n # -- show data value\n !showdataValue\tshow=0,fontsize=12,fontitalic=0,textalign=start\n\n")
+	for i,line in enumerate(doublematrix):
+		evhm.write("%s\t" %config.motiflist[i])
+		linew = ""
+		for l in line:
+			linew += str(l)
+			linew += ','
+		evhm.write("%s\n" %linew[:-1])
+	evhm.close()
+
 
 
 for combo in orthin:
@@ -74,12 +99,29 @@ for s in substitutions:
 	if substitutions[s] != 0:
 		a,b = list(s)
 		#print s, substitutions[s], individual[a], individual[b]
-#print ambiguous
-for a in ambiguous:
-	if ambiguous[a] != 0:
-		print a, ambiguous[a], orthambi[a], str(int(float(orthambi[a])/ambiguous[a] * 100))+'%'
-#print orthambi
-	
-	
-# then check if they are conserved (per mot combo: yes or no) -- go over both lists, so yes orthologs will be counted double!
-# 
+
+#MAKE THE HEATMAP FOR AMBIGUOUS APPEARANCE#
+doublematrix1,doublematrix2 = [],[]
+for m in config.motiflist:
+	line1,line2 = [],[]
+	for n in config.motiflist:
+		a = config.translationdict[m]
+		b = config.translationdict[n]
+		combo = frozenset([a,b])
+		line1.append(ambiguous[combo])
+		if ambiguous[combo] > 50:
+			freq = float(orthambi[combo])/ambiguous[combo]
+		else:
+			freq = 0
+		line2.append(freq)
+	doublematrix1.append(line1)
+	doublematrix2.append(line2)
+makeevolviewheatmap(doublematrix1,"ambiguous")
+makeevolviewheatmap(doublematrix2,"orthambi")
+
+totalambiguous,totalconserved = 0,0
+for key in ambiguous:
+	totalambiguous += ambiguous[key]
+	totalconserved += orthambi[key]
+
+print "ambiguous: %s, of which conserved: %s (%s" %(totalambiguous,totalconserved,int(float(totalconserved)/totalambiguous*100)) + "%)"
