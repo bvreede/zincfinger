d189e630930406e19f5b68bb4c8a7f9f66512050
Barbara Vreede <b.vreede@gmail.com>
2015-08-29 20:44:09 +0200

    removed pipeline from repo

--- pipeline/fixnewic.py
+++
@@ -1,9 +0,0 @@
-infile = open("/home/barbara/Dropbox/shared_work/zinc_finger_data/data/results/motifhits_allz.txt")
-outfile = open("/home/barbara/Dropbox/shared_work/zinc_finger_data/data/results/motifhits_allz2.txt", "w")
-
-
-for line in infile:
-	line = line.replace("dm:", "dm-")
-	outfile.write(line)
-
-outfile.close()
